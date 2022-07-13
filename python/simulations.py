df28fd99fc2acac6dc1f286cfb88b7fdea4935e1
noamroth <39137729+noamroth@users.noreply.github.com>
2022-07-13 14:46:33 -0700

    Create simulations.py

---
+++ python/simulations.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 14:40:23 2022
+
+@author: Noam Roth
+
+code to run simulations for slidingRefractory metric
+"""
+
+
+def genST(rate, params = None):
+    '''
+    
+
+    Parameters
+    ----------
+    rate : float
+        DESCRIPTION.
+    params : dict, optional
+        Todo: here include any parameters like bursty, drifty? The default is None.
+
+    Returns
+    -------
+    spikeTrain: np.array
+        array of spike times in seconds. 
+
+    '''
+    
+    return spikeTrain
