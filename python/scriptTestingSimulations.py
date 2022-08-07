6c798cdc19b8005fb2df4a9e4669b1740d020f14
noamroth <39137729+noamroth@users.noreply.github.com>
2022-08-07 16:19:13 -0700

    testing densing parameters

--- python/scriptTestingSimulations.py
+++ python/scriptTestingSimulations.py
@@ -8,7 +8,19 @@ script for running and plotting various simulations
 """
 
 import simulations
+from simulations import *
 import pickle
+import numpy as np
+from phylib.stats import correlograms
+from types import SimpleNamespace
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+from scipy import stats
+from scipy.optimize import curve_fit
+import time
+from slidingRP import *
+
 
 #%%
 #script testing (to be moved later for import purposes)
@@ -34,11 +46,11 @@ combST = np.sort(np.concatenate((st, contST)))
 #%%
 sampleRate = 30000
 params = {
-    'recDurs': np.array([1, 2, 5]),#np.array([0.25, 0.5, 1, 2, 5]),  #recording durations (hours)
-    'RPs': np.array([0.001, 0.002, 0.003]), #np.array([0.001,0.0015, 0.002, 0.003, 0.004, 0.005]), #true RP (s)
-    'baseRates': np.array([ 0.25, 0.5, 1, 5, 10]), #F1, 2, 5, 10 , 20 R (spk/s)
-    'contRates':  np.arange(0.00,0.225, 0.05), #contamination levels (proportion) #.025
-    'nSim': 10,
+    'recDurs': np.array([0.25, 0.5, 1, 2, 5]),  #recording durations (hours)
+    'RPs': np.array([0.001,0.0015, 0.002, 0.0025, 0.003, 0.004, 0.005]), #true RP (s)
+    'baseRates': np.array([0.1, 0.25, 0.5, 1, 5, 10,20]), #F1, 2, 5, 10 , 20 R (spk/s)
+    'contRates':  np.arange(0.00,0.225, 0.01), #contamination levels (proportion) #.025
+    'nSim': 20,
     'threshold': 0.1,
     'binSize': 1 / sampleRate,
     'sampleRate': 30000,  #TODO figure out a way to refer to this in binsize?
@@ -125,12 +137,19 @@ lam = P* R * D * S * S
 C = 1-stats.poisscdf(B, lam)
 
 #%%
+
+#define params
 contRate = 0.1
 rp = 0.002
 baseRate = 4
 
 recDur = 2*3600
 nSim = 1000
+
+
+rpBinSize = 1 / 30000  
+rpEdges = np.arange(0, 10/1000, rpBinSize) # in s  
+rpVec = rpEdges + np.mean(np.diff(rpEdges)[0])/2 #
 
 obsViol = np.empty(nSim)
 obsViol[:] = np.nan
@@ -143,7 +162,7 @@ for i in range(nSim):
     isi = np.delete(isi,np.where(isi<rp)[0]) #get rid of already contaminating spikes (does this make sense??? why are there already contaminating spikes)
     st = np.cumsum(isi)
     if contRate>0: #contamination greater than 0 
-        contST = genST(contRate,recDur,params)
+        contST = genST(contRate*baseRate,recDur,params)
     else:
         contST=[]
     combST = np.sort(np.concatenate((st, contST))) # put spike times from both spike trains together (and sort chronologically)
@@ -152,33 +171,71 @@ for i in range(nSim):
     spikeCount = len(st)
     
     
-    # [maxConfidenceAt10Cont, minContWith90Confidence, timeOfLowestCont,
-    #      nSpikesBelow2, confMatrix, cont, rpVec, nACG,
-    #      firingRate, secondsElapsed] = slidingRP(combST, params)
-    
-    # conf = 100*computeViol(obsViol, firingRate, 
-    #                           spikeCount, rp[rpIdx]+rpBinSize/2, cont[cidx]/100)
-    
-    
-    
-    
+    #compute firing rate 
     clustersIds = [0] # call the cluster id 0 (not used, but required input for correlograms)
     spikeClustersACG = np.zeros(len(st), dtype = 'int8') # each spike time gets cluster id 0 
     nACGFR = correlograms(st, spikeClustersACG, cluster_ids = clustersIds, bin_size = 1, sample_rate = 30000, window_size=2,symmetrize=False)[0][0] #compute acg
     firingRate = nACGFR[1] / spikeCount
-        
+      
+    #compute ACG
     nACG = correlograms(st, spikeClustersACG, cluster_ids = clustersIds, bin_size = params['binSizeCorr'], sample_rate = params['sampleRate'], window_size=2,symmetrize=False)[0][0] #compute acg
-
-   
-    # compute observed violations  #rp[61] = 0.00205
-    obsViol[i] = sum(nACG[0:61]) #TODO this is off slightly (half-bin) from matlab...
+  
+    # compute observed violations  
+    rpIdx = np.where(rpVec > rp)[0][0]
+    obsViol[i] = sum(nACG[0:rpIdx])
     # print('obsViol: ', obsViol)
     
     
     expectedViol[i] = firingRate * contRate * rp * 2 * spikeCount 
     # print('expectedViol: ', expectedViol)
     
+#%%
 
+#%% test the 'expected count'
+
+baseRates = np.logspace(-0.6,1.5,80)
+recDur = 7200; 
+rp = 0.002;
+contProp = 0.1;
+# params = struct(); params.cont = 10;
+obsViol = np.empty(len(baseRates))
+obsViol[:] = np.nan
+expViol = np.empty(len(baseRates))
+expViol[:] = np.nan
+for bidx in range(len(baseRates)):
+    baseRate = baseRates[bidx];
+    contRate = baseRate*contProp;
+    
+    st = genST(baseRate,recDur,params);  
+    isi = np.diff(np.insert(st,0,0)) 
+    isi = np.delete(isi,np.where(isi<rp)[0]) #get rid of already contaminating spikes (does this make sense??? why are there already contaminating spikes)
+    st = np.cumsum(isi)
+    
+    
+    
+    contST = genST(contRate,recDur,params)
+    
+    combST = np.sort(np.concatenate((st, contST))) # put spike times from both spike trains together (and sort chronologically)
+    
+    [ confMatrix, cont, rpTestVals, nACG, firingRate]= computeMatrix(combST, params)
+    
+    nACG = nACG[0:len(rpTestVals)]
+    contaminationRate = firingRate*contProp; 
+    expectedViol = contaminationRate*rp*2*len(combST);
+    
+    obsViol[bidx] = sum(nACG[rpTestVals<=0.002]); 
+    expViol[bidx] = expectedViol;
+
+#%%
+fig,ax= plt.subplots(1,1)
+ax.plot(baseRates, obsViol, 'r.-',label = 'observed violations'); 
+ax.plot(baseRates, expViol, 'k.-',label = 'expected violations');
+ax.set_xlabel('Base firing rate (spks/s)')
+ax.set_ylabel('Number of violations'); 
+ax.spines.right.set_visible(False)
+ax.spines.top.set_visible(False)
+fig.legend()
+#%%
 
 
 
