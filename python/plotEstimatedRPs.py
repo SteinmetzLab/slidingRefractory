a755e60470eda247d665e463bffdcfc541010be6
noamroth <39137729+noamroth@users.noreply.github.com>
2022-08-24 10:20:36 -0700

    changed hists to smoothed gaussian

--- python/plotEstimatedRPs.py
+++ python/plotEstimatedRPs.py
@@ -238,51 +238,83 @@ plt.suptitle('Steinmetz 2019',y=1.1)
 fig.show()
 
  #%% polot allen       
-import pickle
-#now save for use in iblenv
-filename = r'C:\Users\Steinmetz Lab User\Documents\GitHub\analysis\metrics\slidingRP\ts_region_dict_all.pkl'
-
-file = open(filename,'rb')
-
-ts_dict = pickle.load(file)
-file.close()
-
-
-savefile = r'C:\Users\Steinmetz Lab User\Documents\GitHub\analysis\slidingRefractory\python\savedAllenFits\\'
-
 rpBinSize = 1 / 30000  
 rpEdges = np.arange(0, 10/1000, rpBinSize) # in s  
 rp = rpEdges + np.mean(np.diff(rpEdges)[0])/2 # vector of refractory period durations to test 
 params = {}
+nSess = len(sessions)
+plotEach = False
+cortexAll = []
+hippocampusAll = []
+thalamusAll = []
+midbrainAll = []
 
-for b, brainRegion in enumerate(ts_dict.keys()):
-    #load saved rpMetrics
-    try:
-        file = open(savefile + brainRegion + '.pickle','rb')
-    except:
-        continue
-    rpEstimates = pickle.load(file)
-    file.close()
+#find session names (from the saved files)
+from os import listdir
+from os.path import isfile, join
+mypath = 'E:\AllenBrainObservatory\saved_units'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+sessions = [i[11:20] for i in onlyfiles]
 
-    if brainRegion == 'hippocampus':
-        hippocampus  = np.array([rpEstimates['rpEstimate'][i] for i in range(len(rpEstimates['brainRegion'])) if rpEstimates['brainRegion'][i] == 'hippocampus'])
-    elif brainRegion == 'cortex':
-        cortex  = np.array([rpEstimates['rpEstimate'][i] for i in range(len(rpEstimates['brainRegion'])) if rpEstimates['brainRegion'][i] == 'cortex'])
-    elif brainRegion == 'thalamus':
-        thalamus= np.array([rpEstimates['rpEstimate'][i] for i in range(len(rpEstimates['brainRegion'])) if rpEstimates['brainRegion'][i] == 'thalamus'])
-    elif brainRegion == 'midbrain':
-        midbrain = np.array([rpEstimates['rpEstimate'][i] for i in range(len(rpEstimates['brainRegion'])) if rpEstimates['brainRegion'][i] == 'midbrain'])#,'SNr','APN', 'IC','MB','MRN', 'NB','PAG','RN','SCig', 'SCm',  'SCs', 'SCsg']])
+for j, session_id in enumerate(sessions):
+    file_name = r'C:\Users\Steinmetz Lab User\Documents\GitHub\analysis\slidingRefractory\python\savedAllenFits\%s.pickle'%session_id
+    print('loading data for session %d out of %d'%(j+1, len(sessions)))
+
+    #load the dataframe for this session
+    with open(file_name, 'rb') as f:
+        rpEstimates = pickle.load(f)
+
+
+    hippocampus = np.array([rpEstimates['rpEstimate'][i] for i in range(len(rpEstimates['brainRegion'])) if rpEstimates['parentRegion'][i] == 'hippocampus'])
+    cortex  = np.array([rpEstimates['rpEstimate'][i] for i in range(len(rpEstimates['brainRegion'])) if rpEstimates['parentRegion'][i] == 'cortex'])
+    thalamus= np.array([rpEstimates['rpEstimate'][i] for i in range(len(rpEstimates['brainRegion'])) if rpEstimates['parentRegion'][i] == 'thalamus'])
+    midbrain = np.array([rpEstimates['rpEstimate'][i] for i in range(len(rpEstimates['brainRegion'])) if rpEstimates['parentRegion'][i] == 'midbrain'])
     
-hippocampus[hippocampus<.05] =np.nan
-cortex[cortex<.05] = np.nan
-thalamus[thalamus<.05] =np.nan
-midbrain[midbrain<.05] =np.nan
+    hippocampus[hippocampus<.05] =np.nan
+    thalamus[thalamus<.05] = np.nan
+    cortex[cortex<.05] =np.nan
+    midbrain[midbrain<.05] =np.nan
+    
+    if plotEach:
+        fig,axs = plt.subplots(2,2,figsize = (12,10))
+        ax = axs[0,0]
+        ax.hist(np.concatenate((ca1, dg)), 100)
+        ax.set_title('hippocampus median: %.2f'%np.nanmedian(np.concatenate((ca1, dg))))
+        ax = axs[0,1]
+        ax.hist(np.concatenate((po, lp)), 100)
+        ax.set_title('thalamus median: %.2f'%np.nanmedian(np.concatenate((po, lp))))
+        
+        ax = axs[1,0]
+        ax.hist(visa, 100)
+        ax.set_title('cortex median: %.2f'%np.nanmedian(visa))
+    
+    if len(hippocampusAll) == 0:
+        hippocampusAll = hippocampus
+    else:    
+        hippocampusAll = np.concatenate((hippocampusAll, hippocampus))
+        
+    if len(cortexAll) == 0:
+        cortexAll = cortex
+    else:
+        cortexAll = np.concatenate((cortexAll, cortex))
+
+    if len(midbrainAll) == 0:
+        midbrainAll = midbrain
+    else:
+        midbrainAll = np.concatenate((midbrainAll, midbrain))
+        
+    if len(thalamusAll) == 0:
+        thalamusAll = thalamus
+    else:
+        thalamusAll = np.concatenate((thalamusAll, thalamus))
+
+        
+cortexAllAllen = cortexAll
+thalamusAllAllen = thalamusAll
+hippocampusAllAllen = hippocampusAll
+midbrainAllAllen = midbrainAll
 
 
-cortexAllAllen = cortex
-thalamusAllAllen = thalamus
-hippocampusAllAllen = hippocampus
-midbrainAllAllen = midbrain
 
 #%%        
 fig,axs = plt.subplots(2,2,figsize = (8,6))
@@ -516,7 +548,7 @@ def plot_metric(data, bins, x_axis_label, color, max_value=-1):
     [plt.gca().spines[loc].set_visible(False) for loc in ['right', 'top', 'left']]
     if max_value < np.max(y) * 1.1:
         max_value = np.max(y) * 1.1
-    plt.ylim([0, max_value])
+    # plt.ylim([0, max_value])
     
     return max_value
 
@@ -530,3 +562,98 @@ for idx, region in enumerate(region_dict.keys()):
     max_value = plot_metric(data, bins, 'log$_{10}$ firing rate (Hz)', color_dict[region], max_value)
     
 _ = plt.legend(region_dict.keys())
+
+
+
+#%%
+from scipy.ndimage.filters import gaussian_filter1d
+
+nBins = 20
+fig,axs = plt.subplots(1,1,figsize = (5,3))
+ax = axs
+
+def plot_hists(ax, data, color, linestyle, label= None):
+    cc = data[~np.isnan(data)]
+    xx = sum(~np.isnan(data))
+    h, b = np.histogram(cc, bins=nBins, weights = np.ones(xx) / xx, density=False)
+    x = b[:-1]
+    y = gaussian_filter1d(h, 1)
+    ax.plot(x, y, color=color,linestyle = linestyle, label = label)
+
+plot_hists(ax, cortexAllRS, 'blue', '-', 'cortex')
+plot_hists(ax, thalamusAllRS, 'green', '-', 'thalamus')
+plot_hists(ax, hippocampusAllRS, 'purple', '-', 'hippocampus')
+
+plot_hists(ax, cortexAllSteinmetz, 'blue', '--')
+plot_hists(ax, thalamusAllSteinmetz, 'green', '--')
+plot_hists(ax, hippocampusAllSteinmetz, 'purple', '--')
+
+plot_hists(ax, cortexAllAllen, 'blue', ':')
+plot_hists(ax, thalamusAllAllen, 'green', ':')
+plot_hists(ax, hippocampusAllAllen, 'purple', ':')
+
+ax.set_xlabel('Estimated RP (ms)')
+ax.set_ylabel('Proportion of neurons')
+ax.spines.right.set_visible(False)
+ax.spines.top.set_visible(False)
+
+
+#add arrows:
+lenArrow = .03
+lenHead = .01
+wiArrow = .1
+ind = np.nanmedian(cortexAllRS)
+n = .25#len(cortexAll[(cortexAll > ind*0.95) & (cortexAll <  ind*1.05)])/2 #y value of overall histogram
+ax.arrow(ind, n+lenArrow+lenHead, 0, -lenArrow, head_width=wiArrow*3, head_length=lenHead, width=wiArrow, fc='blue', ec='none',linestyle = '-')
+ind = np.nanmedian(cortexAllSteinmetz)
+ax.arrow(ind, n+lenArrow+lenHead, 0, -lenArrow, head_width=wiArrow*3, head_length=lenHead, width=wiArrow, fc='blue', ec='none',linestyle = '--')
+ind = np.nanmedian(cortexAllAllen)
+ax.arrow(ind, n+lenArrow+lenHead, 0, -lenArrow, head_width=wiArrow*3, head_length=lenHead, width=wiArrow, fc='blue', ec='none',linestyle = ':')
+
+ind = np.nanmedian(thalamusAllRS)
+# n = len(thalamusAll[(thalamusAll > ind*0.95) & (thalamusAll <  ind*1.05)])/2 #y value of overall histogram
+ax.arrow(ind, n+lenArrow+lenHead, 0, -lenArrow, head_width=wiArrow*3, head_length=lenHead, width=wiArrow, fc='green', ec='none',linestyle = '-')
+ind = np.nanmedian(thalamusAllSteinmetz) - .4
+ax.arrow(ind, n+lenArrow+lenHead, 0, -lenArrow, head_width=wiArrow*3, head_length=lenHead, width=wiArrow, fc='green', ec='none',linestyle = 'dashed')
+ind = np.nanmedian(thalamusAllAllen)
+ax.arrow(ind, n+lenArrow+lenHead, 0, -lenArrow, head_width=wiArrow*3, head_length=lenHead, width=wiArrow, fc='green', ec='none',linestyle = ':')
+
+ind = np.nanmedian(hippocampusAllRS)
+# n = len(hippocampusAll[(hippocampusAll > ind*0.95) & (hippocampusAll <  ind*1.05)])/2 #y value of overall histogram
+ax.arrow(ind, n+lenArrow+lenHead, 0, -lenArrow, head_width=wiArrow*3, head_length=lenHead, width=wiArrow, fc='purple', ec='none',linestyle = '-')
+ind = np.nanmedian(hippocampusAllSteinmetz)
+ax.arrow(ind, n+lenArrow+lenHead, 0, -lenArrow, head_width=wiArrow*3, head_length=lenHead, width=wiArrow, fc='purple', ec='none',linestyle = '--')
+ind = np.nanmedian(hippocampusAllAllen)
+ax.arrow(ind, n+lenArrow+lenHead, 0, -lenArrow, head_width=wiArrow*3, head_length=lenHead, width=wiArrow, fc='purple', ec='none',linestyle = ':')
+
+
+
+
+
+
+
+ax.plot(np.NaN, np.NaN, '-', color='black', label='IBL')
+ax.plot(np.NaN, np.NaN, '--', color='black', label='Steinmetz 2019')
+ax.plot(np.NaN, np.NaN, ':', color='black', label='Allen')
+
+plt.legend(frameon=False)
+plt.tight_layout()
+# plt.suptitle('All Datasets',y=1.1)
+fig.show()
+
+plt.savefig(r'C:\Users\Steinmetz Lab User\Documents\GitHub\analysis\slidingRefractory\python\estimatedRPs.pdf', dpi=300, format='pdf')
+# cortexAllRS, cortexAllSteinmetz, cortexAllAllen
+
+#%%
+
+xx = sum(~np.isnan(thalamusAllRS))
+tt = thalamusAllRS[~np.isnan(thalamusAllRS)]
+ax.hist(tt, nBins,weights = np.ones(xx) / xx, histtype = 'step', color = 'green', label = 'Thalamus', linestyle = '-')
+
+xx = sum(~np.isnan(hippocampusAllRS))
+hh= hippocampusAllRS[~np.isnan(hippocampusAllRS)]
+
+ax.hist(hh, nBins, weights = np.ones(xx) /xx, histtype = 'step', color = 'purple', label = 'Hippocampus', linestyle = '-')
+ax.set_ylim(0,.3)
+# plt.hist(data, weights=np.ones(len(data)) / len(data))
+
