596a0adffd51d028d999be6afeaad3b2895824b9
noamroth <noamroth@gmail.com>
2023-09-04 12:12:11 -0700

    Changed expectedViol to "newcalc"

--- python/slidingRP/metrics.py
+++ python/slidingRP/metrics.py
@@ -302,7 +302,11 @@ def computeViol(obsViol, firingRate, spikeCount, refDur, contaminationProp):
     contaminationRate = firingRate * contaminationProp
 
     # the number of violations (spikes) we expect to see under this contamination rate
-    expectedViol = contaminationRate * refDur * 2 * spikeCount
+    #expectedViol = contaminationRate * refDur * 2 * spikeCount
+    #as computed *not* in the same way as Hill (see above commented out)
+    N_b = firingRate * recDur
+    N_c = contaminationRate * recDur
+    expectedViol = 2 * refDur * 1/recDur * N_c * (N_b + (N_c - 1)/2)
 
     # the confidence th-at this neuron is contaminated at a level less than contaminationProp, given the number of true
     # observed violations and under the assumption of Poisson firing
