5ac67c0bb5f2cf623720f326c22630c4c1be7025
noamroth <noamroth@gmail.com>
2023-12-22 11:17:57 -0800

    set up flags for rerunning

--- python/slidingRP/scriptSavePaperFigs.py
+++ python/slidingRP/scriptSavePaperFigs.py
@@ -6,13 +6,27 @@ resultsBasePath = r'C:\Users\noamroth\int-brain-lab\slidingRefractory\python\sli
 
 import os #import to create folders for each figure
 
+
+#%%
+#Set rerun flag:
+rerunAllSims = False
+
+if rerunAllSims = True:
+    rerunFig3 = True
+    rerunFig4 = True
+    rerunFig5 = True
+    rerunFig6 = True
+    rerunFig7 = True
+
+
+
 #%% Figure 1: #TODO
 
 #%% Figure 2: Metric algorithm
 from slidingRP.Fig2_ACGMetric import * #import code to make this figure
 figsavepath = figSaveBasePath + '\\figure2' #destination folder for all subfigures
 os.makedirs(figsavepath,exist_ok=True) #if this folder doesn't yet exist, create it
-savedSimNeuronFlag=False #set to true to load a previously saved simulated contaminated neuron
+savedSimNeuronFlag=True #set to true to load a previously saved simulated contaminated neuron
 runSaveFig2(figsavepath,resultsBasePath,savedSimNeuronFlag=savedSimNeuronFlag) #run code and save figure to figsavepath
 
 
@@ -21,7 +35,10 @@ runSaveFig2(figsavepath,resultsBasePath,savedSimNeuronFlag=savedSimNeuronFlag) #
 from slidingRP.Fig3_simulations import *
 figsavepath = figSaveBasePath + '\\figure3'
 os.makedirs(figsavepath,exist_ok=True)
-runSaveFig3(figsavepath,resultsBasePath)
+
+
+rerunFig3 = False
+runSaveFig3(figsavepath,resultsBasePath, rerunFig3 = rerunFig3)
 
 
 
@@ -29,8 +46,10 @@ runSaveFig3(figsavepath,resultsBasePath)
 from slidingRP.Fig4_HillCompConfidence import *
 figsavepath = figSaveBasePath + '\\figure4'
 os.makedirs(figsavepath,exist_ok=True)
+
+rerunFig4 = False
 print('Running Figure 4')
-runSaveFig4(figsavepath,resultsBasePath)
+runSaveFig4(figsavepath,resultsBasePath, rerunFig4 = rerunFig4)
 print('Figure 4 saved')
 
 
@@ -39,8 +58,10 @@ print('Figure 4 saved')
 from slidingRP.Fig5_ConfROC import *
 figsavepath = figSaveBasePath + '\\figure5'
 os.makedirs(figsavepath,exist_ok=True)
+
+rerunFig5 = False
 # runSaveFig5_ab(figsavepath)
-runSaveFig5_ROC(figsavepath,resultsBasePath)
+runSaveFig5_ROC(figsavepath,resultsBasePath, rerunFig5 = rerunFig5)
 
 
 
@@ -48,9 +69,11 @@ runSaveFig5_ROC(figsavepath,resultsBasePath)
 # from slidingRP.Fig6_drift import *
 # figsavepath = figSaveBasePath + '\\figure6'
 # os.makedirs(figsavepath,exist_ok=True)
-# runSaveFig6(figsavepath)
+
+rerunFig6 = False
+# runSaveFig6(figsavepath, resultsBasePath, rerunFig6 = rerunFig6)
 
 # Figure 7:
 
-
+rerunFig7 = False
 
